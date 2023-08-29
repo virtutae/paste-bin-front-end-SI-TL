a54a322de85a686c45e8a9e558cb45e5f3568fc8
TomaszLaboj <tomasz.laboj@gmail.com>
2023-08-29 15:36:58 +0100

    fixed tests

--- src/components/InputPage.test.tsx
+++ src/components/InputPage.test.tsx
@@ -4,7 +4,7 @@ import { render, screen } from "../testUtils/testUtils";
 describe("MyComponent", async () => {
     test("Should have text Hello from My Component on it", () => {
         render(<InputPage />);
-        const elem = screen.getByText("Title:");
+        const elem = screen.getByText("Title(optional):");
         expect(elem).toBeInTheDocument();
     });
 });
