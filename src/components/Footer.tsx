827430e4d7192d5f1fda63658d330ab44936cfa8
virtutae <virtutae@yahoo.it>
2023-08-30 11:39:20 +0100

    added hyperlink to front and back end repos and formatted

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,21 @@ function Footer(): JSX.Element {
     return (
         <>
             <div>
-                <a href="https://github.com/virtutae/paste-bin-front-end-SI-TL" target="_blank" rel="noreferrer" >Front end repo</a>
+                <a
+                    href="https://github.com/virtutae/paste-bin-front-end-SI-TL"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Front end repo
+                </a>
                 <br />
-                <a href="https://github.com/virtutae/paste-bin-SI-TL" target="_blank" rel="noreferrer">Back end repo</a>
+                <a
+                    href="https://github.com/virtutae/paste-bin-SI-TL"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Back end repo
+                </a>
             </div>
         </>
     );
