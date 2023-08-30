2c158bd6e66bd733a093e28937f1a1a1546be56d
TomaszLaboj <tomasz.laboj@gmail.com>
2023-08-30 15:15:33 +0100

    added states for inputs and functions for them

--- src/components/InputPage.tsx
+++ src/components/InputPage.tsx
@@ -1,11 +1,32 @@
 import { useState } from "react";
+import { entry } from "./interfaces";
 
 function InputPage(): JSX.Element {
     const [currentTitle, setCurrentTitle] = useState("");
-    function handleTitleInput(e: string) {
-        setCurrentTitle(e);
-        console.log(e);
+    const [currentText, setCurrentText] = useState("");
+    const [currentEntries, setCurrentEntries] = useState<entry[]>([]);
+
+    function handleTitleInput(title: string) {
+        setCurrentTitle(title);
     }
+
+    function handleText(text: string) {
+        setCurrentText(text);
+    }
+
+    function handleSubmit() {
+        const oneEntry: entry = {
+            title: currentTitle,
+            text: currentText,
+        };
+
+        if (currentText.trim().length > 0) {
+            setCurrentEntries([...currentEntries, oneEntry]);
+        } else {
+            alert("Text body cannot be empty!");
+        }
+    }
+    console.log(currentEntries);
 
     return (
         <>
@@ -17,9 +38,13 @@ function InputPage(): JSX.Element {
                 ></input>
                 <br />
                 <div>Paste your code here:</div>
-                <textarea className="textBody"></textarea>
+                <textarea
+                    className="textBody"
+                    onChange={(e) => handleText(e.target.value)}
+                    value={currentText}
+                ></textarea>
                 <br />
-                <button> Add</button>
+                <button onClick={handleSubmit}>Submit</button>
             </div>
         </>
     );
